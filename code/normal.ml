292889952b25cd83ccd910b947913798aa7c82b2
yoma <ym.atonement.oy999@gmail.com>
2019-10-22 02:00:39 +0900

    refactoring Loop Recur Tuple

--- code/normal.ml
+++ code/normal.ml
@@ -111,16 +111,16 @@ let rec norm_exp (e: Syntax.exp) (f: cexp -> exp) = match e with
     | S.BLit false -> f (ValExp (IntV 0))
 
     | S.BinOp(op, e1, e2) -> let nid1 = fresh_id "ai" in let nid2 = fresh_id "bi" in
-            norm_exp e1 (fun x -> 
-                (match x with
-                        ValExp vc1 -> norm_exp e2 (fun y -> 
-                                        (match y with
-                                                ValExp vc2 -> f (BinOp(op, vc1, vc2))
-                                            |   _          -> LetExp(nid2, y, f (BinOp(op, vc1, Var nid2)))))
-                    |   _          -> norm_exp e2 (fun y -> 
-                                        (match y with
-                                                ValExp vc2 -> LetExp(nid1, x, f (BinOp(op, Var nid1, vc2)))
-                                            |   _          -> LetExp(nid1, x, LetExp(nid2, y, f (BinOp(op, Var nid1, Var nid2))))))))
+        norm_exp e1 (fun x -> 
+            (match x with
+                    ValExp vc1 -> norm_exp e2 (fun y -> 
+                                    (match y with
+                                            ValExp vc2 -> f (BinOp(op, vc1, vc2))
+                                        |   _          -> LetExp(nid2, y, f (BinOp(op, vc1, Var nid2)))))
+                |   _          -> norm_exp e2 (fun y -> 
+                                    (match y with
+                                            ValExp vc2 -> LetExp(nid1, x, f (BinOp(op, Var nid1, vc2)))
+                                        |   _          -> LetExp(nid1, x, LetExp(nid2, y, f (BinOp(op, Var nid1, Var nid2))))))))
 
 	| S.IfExp (e1, e2, e3) ->	
         (match e1 with
@@ -152,29 +152,25 @@ let rec norm_exp (e: Syntax.exp) (f: cexp -> exp) = match e with
 
     | S.LetRecExp (id1, id2, e1, e2) -> LetRecExp (id1, id2, norm_exp e1 (fun x -> CompExp x), norm_exp e2 f)
 
-	| S.LoopExp (id, e1, e2) -> 
-        (match e1 with
-                S.LetExp (_, _, _) | S.LetRecExp (_, _, _, _) | S.LoopExp (_, _, _) | S.RecurExp _ ->
-                    let nid = fresh_id "va" in
-                    norm_exp e1 (fun x -> LetExp (nid, x, LoopExp (id, ValExp (Var nid), norm_exp e2 f)))
-            | _ -> norm_exp e1 (fun x -> LoopExp (id, x, norm_exp e2 f)))
+    | S.LoopExp (id, e1, e2) -> norm_exp e1 (fun x -> LoopExp (id, x, norm_exp e2 f))
+
     | S.RecurExp e -> 
-        (match e with
-                S.ILit _ | S.BLit _ -> RecurExp (con_expvalue e)
-            |   _ -> let nid = fresh_id "va" in 
-                        norm_exp e (fun x -> LetExp (nid, x, RecurExp (Var nid))))
-    | S.TupleExp (e1, e2) -> 
-        (match e1 with
-                S.ILit _ | S.BLit _ -> let e1v = con_expvalue e1 in
-                                                    (match e2 with
-                                                            S.ILit _ | S.BLit _ -> f (TupleExp (e1v, con_expvalue e2))
-                                                        |   _ -> let nid2 = fresh_id "va" in
-                                                                    norm_exp e2 (fun x -> LetExp (nid2, x, f (TupleExp (e1v, Var nid2)))))
-            |   _ -> let nid1 = fresh_id "va" in
-                        (match e2 with
-                                S.ILit _ | S.BLit _ -> norm_exp e1 (fun x -> LetExp (nid1, x, f (TupleExp (Var nid1, con_expvalue e2))))
-                            |   _ -> let nid2 = fresh_id "va" in
-                                        norm_exp e1 (fun x -> norm_exp e2 (fun y -> LetExp (nid1, x, LetExp (nid2, y, f (TupleExp (Var nid1, Var nid2))))))))
+        norm_exp e (fun x -> (match x with
+                                    ValExp vc -> RecurExp vc
+                                |   _         -> let nid = fresh_id "hi" in LetExp (nid, x, RecurExp (Var nid))))
+
+    | S.TupleExp (e1, e2) -> let nid1 = fresh_id "ii" in let nid2 = fresh_id "ji" in
+        norm_exp e1 (fun x -> 
+            (match x with
+                    ValExp vc1 -> norm_exp e2 (fun y -> 
+                                    (match y with
+                                            ValExp vc2 -> f (TupleExp(vc1, vc2))
+                                        |   _          -> LetExp (nid2, y, f (TupleExp (vc1, Var nid2)))))
+                |   _          -> norm_exp e2 (fun y -> 
+                                    (match y with
+                                            ValExp vc2 -> LetExp (nid1, x, f (TupleExp(Var nid1, vc2)))
+                                        |   _          -> LetExp (nid1, x, LetExp (nid2, y, f (TupleExp(Var nid1, Var nid2))))))))
+
     | S.ProjExp (e, i) ->
         (match e with
                 S.ILit _ | S.BLit _ -> f (ProjExp (con_expvalue e, i))
